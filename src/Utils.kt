5d34a377034fc2f0b2e77499e5195526c3762381
Blake <macdadeblake@gmail.com>
2023-12-05 20:49:12 +1300

    Update utils

--- src/Utils.kt
+++ src/Utils.kt
@@ -19,3 +19,7 @@ fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteA
  * The cleaner shorthand for printing output.
  */
 fun Any?.println() = println(this)
+
+fun String.toIntList() = this.trim().split(" ").filter { it.isNotBlank() }.map{it.toInt()}
+
+fun String.toLongList() = this.trim().split(" ").filter { it.isNotBlank() }.map{it.toLong()}
